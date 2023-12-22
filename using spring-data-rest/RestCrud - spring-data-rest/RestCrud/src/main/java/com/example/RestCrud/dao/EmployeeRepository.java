97db18dda874ab5c417b89dc7b545d6ea26ef59f
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-12-22 11:00:23 +0530

    Add files via upload

---
+++ using spring-data-rest/RestCrud - spring-data-rest/RestCrud/src/main/java/com/example/RestCrud/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.RestCrud.dao;
+
+import com.example.RestCrud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// @RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+	// that's it... no need to write any code
+	// no need of implementation class
+
+	// get these methods for free - findByAll(), findById(), save(), deleteById(), others....
+}
